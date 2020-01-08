b6b637821ef57e0be960fb3cf1e55acdc0eb58f2
minhluan1998 <minhluan1998@gmail.com>
2020-01-08 17:15:27 +0700

    init project

---
+++ dictionary-backend/src/main/java/com/luanphm/dictionaryflashcard/DictionaryflashcardApplication.java
@@ -0,0 +1,13 @@
+package com.luanphm.dictionaryflashcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DictionaryflashcardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DictionaryflashcardApplication.class, args);
+	}
+
+}
